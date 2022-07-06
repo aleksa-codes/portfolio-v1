3dd9dcb35c19ba19e67d0f021861309f18b0daa9
aleksastojsic <stojsic.aleksa@gmail.com>
2022-07-06 18:58:11 +0200

    Fix: next config

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const nextConfig = {
 module.exports = {
   ...nextConfig,
   images: {
-    domains: ['picsum.photos']
+    domains: ['picsum.photos'],
+    deviceSizes: [320, 480, 768, 1024, 1280, 1600, 1920, 2560, 3840, 5120, 7680]
   }
 };
