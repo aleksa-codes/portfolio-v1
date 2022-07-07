0996620165a899f8a3c00c909f3b8f37c6c5b0ed
aleksastojsic <stojsic.aleksa@gmail.com>
2022-07-07 23:23:10 +0200

    Fix: repalced script tag with next/script Script and moved coffee script to _doc

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -15,19 +15,6 @@ const Layout = ({ children }: LayoutProps) => {
         </main>
       </div>
       {/* <Footer /> */}
-      <script
-        data-name='BMC-Widget'
-        data-cfasync='false'
-        src='https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js'
-        data-id='aleksa'
-        data-description='Support me on Buy me a coffee!'
-        data-message='Thanks for visiting. Made with 💻 and ☕.'
-        data-color='#79D6B5'
-        data-position='Right'
-        data-x_margin='4'
-        data-y_margin='4'
-        async
-      ></script>
     </>
   );
 };
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,7 @@ import Document, {
   Main,
   NextScript
 } from 'next/document';
+import Script from 'next/script';
 
 const APP_NAME = 'aleksa.codes';
 // const APP_DESCRIPTION = "Aleksa's personal website";
@@ -106,13 +107,28 @@ class MyDocument extends Document {
           />
         </Head>
         <body>
-          <script
+          <Main />
+          <NextScript />
+          <Script
+            id='blockingSetInitialColorMode'
             dangerouslySetInnerHTML={{
               __html: blockingSetInitialColorMode
             }}
-          ></script>
-          <Main />
-          <NextScript />
+            strategy='beforeInteractive'
+          ></Script>
+          <Script
+            data-name='BMC-Widget'
+            data-cfasync='false'
+            src='https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js'
+            data-id='aleksa'
+            data-description='Support me on Buy me a coffee!'
+            data-message='Thanks for visiting. Made with 💻 and ☕.'
+            data-color='#79D6B5'
+            data-position='Right'
+            data-x_margin='4'
+            data-y_margin='4'
+            strategy='beforeInteractive'
+          ></Script>
         </body>
       </Html>
     );
