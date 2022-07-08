e38a88a76e68510d376287672d836531618bbd43
aleksastojsic <stojsic.aleksa@gmail.com>
2022-07-08 21:09:56 +0200

    Fix: homepage title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import Hero from '../components/Hero';
 const Home: NextPage = () => (
   <>
     <Head>
-      <title>aleksa</title>
+      <title>aleksa.codes</title>
       <meta name='robots' content='index,nofollow' />
       <meta name='googlebot' content='index,nofollow' />
       <meta name='description' content="Aleksa's personal website" />
