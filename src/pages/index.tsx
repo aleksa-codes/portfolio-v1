afb23784e5179267740e4c2136d7dd438be89e29
Aleksa <stojsic.aleksa@gmail.com>
2022-08-27 18:48:00 +0200

    Fix: removed from google the old site hoefuly

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,8 +6,8 @@ const Home: NextPage = () => (
   <>
     <Head>
       <title>aleksa.codes</title>
-      <meta name='robots' content='index,nofollow' />
-      <meta name='googlebot' content='index,nofollow' />
+      <meta name='robots' content='noindex,nofollow' />
+      <meta name='googlebot' content='noindex,nofollow' />
       <meta name='description' content="Aleksa's personal website" />
       <link rel='canonical' href='https://aleksa.vercel.app' />
     </Head>
